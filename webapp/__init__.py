dde1c32f0f418e830964ae0e13ea069e6bbd06be
drakedu <drakezdu@gmail.com>
2022-12-04 16:22:08 -0500

    Drake changed __init__.py.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -7,24 +7,21 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app(test_config=None):
-    # create and configure the app
+    # This creates and configures the app.
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'belongr.db'),
-    )
+    app.config.from_mapping(SECRET_KEY='dev', DATABASE=os.path.join(app.instance_path, 'belongr.db'))
 
     db = SQLAlchemy(app)
     db.app = app
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
+        # This loads the instance configuration, if it exists, when not testing.
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
+        # This loads the test configuration if passed in.
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
+    # This ensures the instance folder exists.
     try:
         os.makedirs(app.instance_path)
     except OSError:
